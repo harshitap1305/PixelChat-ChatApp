2c954855f2997aad740a1daa5d963d2f380cc7a2
Harshita Patidar <harshitapatidar257@gmail.com>
2026-09-14 11:51:54 +0530

    max idle connecitons and max idle per host increases

--- load_balancer/main.go
+++ load_balancer/main.go
@@ -275,8 +275,8 @@ func (lb *LoadBalancer) statsLoop(interval time.Duration) {
 var globalTransport = &http.Transport{
 	TLSClientConfig:       &tls.Config{InsecureSkipVerify: true}, // #nosec G402
 	ResponseHeaderTimeout: 10 * time.Second,
-	MaxIdleConns:          600,
-	MaxIdleConnsPerHost:   200,
+	MaxIdleConns:          9000,
+	MaxIdleConnsPerHost:   3000,
 	IdleConnTimeout:       90 * time.Second, // fixed: was 4s, caused TCP churn under load
 	DialContext: (&net.Dialer{
 		Timeout:   5 * time.Second,
